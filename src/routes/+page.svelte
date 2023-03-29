eef909c2deb006b789bcb36fb7692f0dc31e68a9
Jens Andersen <jensra@gmail.com>
2023-03-29 20:46:28 +0200

    First commit.

---
+++ src/routes/+page.svelte
@@ -0,0 +1,45 @@
+<script>
+    import Player from './Player.svelte';
+  
+      let playerList = [4];
+    let activePlayerIndex = -1;
+    
+      function handleMessage(event) {
+          let playerIndex = event.detail.index;
+          if (activePlayerIndex == -1) {
+              playerList[playerIndex].startTimer();
+              activePlayerIndex = playerIndex;
+          } else if (activePlayerIndex === playerIndex) {
+              playerList[playerIndex].stopTimer();
+              activePlayerIndex = -1;
+          } else {
+              for(let i = 0; i < playerList.length; i++) {
+                  playerList[i].stopTimer();
+              }
+              activePlayerIndex = playerIndex;
+              playerList[activePlayerIndex].startTimer();
+          }
+      }
+      
+  </script>
+  
+  <div class="grid">
+    <Player
+        name="Player 1"
+          index=0
+          bind:this={playerList[0]}
+          on:message={handleMessage}
+      />
+    <Player name="Player 2" index=1 bind:this={playerList[1]} on:message={handleMessage}/>
+      <Player name="Player 3" index=2 bind:this={playerList[2]} on:message={handleMessage}/>
+      <Player name="Player 4" index=3 bind:this={playerList[3]} on:message={handleMessage}/>
+  </div>
+  
+  <style>
+    .grid {
+      display: grid;
+      grid-template-columns: repeat(2, 1fr);
+      grid-template-rows: repeat(2, 1fr);
+      gap: 0px;
+    }
+  </style>
