eed7aadfbd3a98a137738634625b623381707076
Jens Andersen <jensra@gmail.com>
2023-04-06 15:59:49 +0200

    Make reset button for default life and time starting values. Align buttons with flex.

--- src/routes/SettingsMenu.svelte
+++ src/routes/SettingsMenu.svelte
@@ -2,6 +2,9 @@
   import { createEventDispatcher } from "svelte";
   import { startingLifeTotal, startingTimeLeftMinutes } from "./stores.js";
 
+  const DEFAULT_STARTING_LIFE = 40;
+  const DEFAULT_STARTING_TIME_MINUTES = 23;
+
   const dispatch = createEventDispatcher();
 
   function restartClicked() {
@@ -14,32 +17,52 @@
     <div>
       Starting life: <input type="number" bind:value={$startingLifeTotal} />
     </div>
-    <button class="value-changer-button"
-      on:click={() => {
-        $startingLifeTotal++;
-      }}>+</button
-    >
-    <button class="value-changer-button"
-      on:click={() => {
-        $startingLifeTotal--;
-      }}>-</button
-    >
+    <div class="value-changer-flex-container">
+      <button
+        class="value-changer-button"
+        on:click={() => {
+          $startingLifeTotal++;
+        }}>+</button
+      >
+      <button
+        class="value-changer-button"
+        on:click={() => {
+          $startingLifeTotal--;
+        }}>-</button
+      >
+      <button
+        class="value-changer-button"
+        on:click={() => {
+          $startingLifeTotal = DEFAULT_STARTING_LIFE;
+        }}>Reset</button
+      >
+    </div>
     <div>
       Starting time: <input
         type="number"
         bind:value={$startingTimeLeftMinutes}
       />
     </div>
-    <button class="value-changer-button"
-      on:click={() => {
-        $startingTimeLeftMinutes++;
-      }}>+</button
-    >
-    <button class="value-changer-button"
-      on:click={() => {
-        $startingTimeLeftMinutes--;
-      }}>-</button
-    >
+    <div class="value-changer-flex-container">
+      <button
+        class="value-changer-button"
+        on:click={() => {
+          $startingTimeLeftMinutes++;
+        }}>+</button
+      >
+      <button
+        class="value-changer-button"
+        on:click={() => {
+          $startingTimeLeftMinutes--;
+        }}>-</button
+      >
+      <button
+        class="value-changer-button"
+        on:click={() => {
+          $startingTimeLeftMinutes = DEFAULT_STARTING_TIME_MINUTES;
+        }}>Reset</button
+      >
+    </div>
   </div>
   <div class="grid-content">Some very clever settings</div>
   <div class="grid-content">
@@ -70,12 +93,19 @@
     margin: auto;
   }
 
+  .value-changer-flex-container {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+    align-items: center;
+  }
+
   .value-changer-button {
     font-size: 3vh;
     border-radius: 10px;
     border: 2px solid rgba(175, 175, 175, 0.6);
     padding: 15px;
-    width: 45%;
+    width: 30%;
   }
 
   .restart-button {
