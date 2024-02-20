ec238441a3cf78eb37ce3dd998aa6a9b1118fef1
Jens Andersen <jensra@gmail.com>
2024-02-20 16:38:11 +0100

    First version of theme selection

---
+++ src/routes/SettingsMenuThemeSelector.svelte
@@ -0,0 +1,70 @@
+<script>
+  let themeNameList = ["orange-brown", "silver-green"];
+  let themeName = themeNameList[0];
+
+  function setTheme(newTheme) {
+    themeName = newTheme;
+  }
+</script>
+
+<svelte:head>
+  {#if themeName === "orange-brown"}
+    <style>
+      :root {
+        --color-active-a: #977612;
+        --color-active-b: #d1a215;
+        --color-inactive-a: #403001;
+        --color-inactive-b: #6b5101;
+        --color-player-dead: #501818;
+      }
+    </style>
+  {:else if themeName === "silver-green"}
+    <style>
+      :root {
+        --color-active-a: #143b15;
+        --color-active-b: #0c4f0e;
+        --color-inactive-a: #646464;
+        --color-inactive-b: #919090;
+        --color-player-dead: #3b1212;
+      }
+    </style>
+  {/if}
+</svelte:head>
+
+<main>
+  <div class="unselectable">
+    <h3>Select color theme:</h3>
+    <div class="theme-button-flex-container">
+    {#each themeNameList as themeId}
+      <button
+        on:click={() => {
+          setTheme(themeId);
+        }}
+      >
+        {themeId}
+      </button>
+    {/each}
+    </div>
+  </div>
+</main>
+
+<style>
+  h3 {
+    margin-top: 5px;
+    margin-bottom: 5px;
+    text-align: center;
+  }
+
+  .theme-button-flex-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-content: center;
+    align-items: center;
+  }
+
+  button {
+    padding: 5px;
+    margin: 10px;
+  }
+</style>
